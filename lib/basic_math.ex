a17b9835517f1a2972baeea634a95a45392e6f73
Pedro Tozzi <pedrogbtozzi@gmail.com>
2024-01-06 23:05:20 -0300

    Add documentation for basic math operations and calculator modules and functions

--- lib/basic_math.ex
+++ lib/basic_math.ex
@@ -1,23 +1,63 @@
 defmodule BasicMath do
+  @moduledoc """
+  Basic math operations
+  """
+
+  @doc """
+  Adds two numbers together
+  ## Examples
+    iex> BasicMath.add(1, 2)
+    3
+  """
   def add(a, b), do: a + b ## Shorthand syntax
 
+  @doc """
+  Subtracts two numbers
+  ## Examples
+    iex> BasicMath.subtract(1, 2)
+    -1
+  """
   def subtract(a, b) do
     a - b
   end
 
+  @doc """
+  Multiplies two numbers
+  ## Examples
+    iex> BasicMath.multiply(1, 2)
+    2
+  """
   def multiply(a, b) do
     a * b
   end
 
+  @doc """
+  Divides two numbers
+  ## Examples
+    iex> BasicMath.divide(1, 2)
+    0.5
+  """
   def divide(a, b) do
     a / b
   end
 
+  @doc """
+  Rounds a number up
+  ## Examples
+    iex> BasicMath.round_up(1.234)
+    1.24
+  """
   def round_up(a) do
     Float.ceil(a, 2)
   end
 
+  @doc """
+  Rounds a number down
+  ## Examples
+    iex> BasicMath.round_down(1.234)
+    1.23
+  """
   def round_down(a) do
-    trunc(a) ## Same as: Float.floor(a)
+    Float.floor(a, 2)
   end
 end
--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,14 +1,36 @@
 defmodule Calculator do
+  @moduledoc """
+  Calculator module
+  """
+
+  @doc """
+  Finds the average of two numbers
+  ## Examples
+    iex> Calculator.find_average(10, 50)
+    30
+  """
   def find_average(a, b) do
-    (BasicMath.add(a, b)) / 2
+    (BasicMath.add(a, b)) / 2 |> trunc
   end
 
+  @doc """
+  Finds the percentage of a value
+  ## Examples
+    iex> Calculator.find_percentage_of_value(10, 50)
+    5.0
+  """
   def find_percentage_of_value(a, b) do
     # decimal_percentage = BasicMath.divide(a, 100)
     # BasicMath.multiply(decimal_percentage, b)
     a |> BasicMath.divide(100) |> BasicMath.multiply(b)
   end
 
+  @doc """
+  Finds the distance traveled
+  ## Examples
+    iex> Calculator.find_distance_travaled(10, 50)
+    500
+  """
   def find_distance_travaled(speed, time) do
     # BasicMath.multiply(speed, time)
     speed |> BasicMath.multiply(time)
